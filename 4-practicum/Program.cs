c6a5b755ef7f8e99d5ccfe19a1af1f023e372b50
User <User@WIN-N8HGK237C16>
2024-10-15 20:27:15 +0300

    Добавьте файлы проекта.

---
+++ 4-practicum/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_practicum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Дано целое число. Если оно является положительным, то прибавить к нему 1; в противном случае, вычесть из него 2.Вывести полученное число на экран.
+
+            //Console.Write("введите целое число: ");
+            //int a = int.Parse(Console.ReadLine());
+            //if (a > 0)
+            //{
+            //    ++a;
+            //}
+            //else
+            //{
+            //    a -= 2;
+            //}
+            //Console.Write(a);
+
+
+            //2.Дано целое число. Если оно является положительным, то прибавить к нему 1; в противном случае, не изменять его.Вывести полученное число на экран.
+
+            //Console.WriteLine("введите целое число");
+            //int a = int.Parse(Console.ReadLine());
+            //if (a > 0)
+            //{
+            //    ++a;
+            //}
+            //Console.Write(a);
+
+
+            //3.Дано целое число. Если оно является положительным, то прибавить к нему 1; если отрицательным, то вычесть из него 2; если нулевым, то заменить его на 10.Вывести полученное число.
+
+            //Console.Write("введите целое число: ");
+            //int a = int.Parse(Console.ReadLine());
+            //if (a > 0)
+            //{
+            //    ++a;
+            //}
+            //else if (a < 0)
+            //{
+            //    a -= 2;
+            //}
+            //else
+            //{
+            //    a = 10;
+            //}
+            //Console.Write(a);
+
+
+            //4.Проверить на четность или нечетность введенное целое число.
+
+            //Console.WriteLine("введите целое число");
+            //int a = int.Parse(Console.ReadLine());
+            //if (a % 2==0)
+            //{
+            //    Console.Write("число четное");
+            //}
+            //else
+            //{
+            //    Console.Write("число четное");
+            //}
+
+
+            //5.Определить, является ли число положительным, отрицательным или нулевым.
+
+            //Console.WriteLine("введите целое число");
+            //int a = int.Parse(Console.ReadLine());
+            //if (a > 0)
+            //{
+            //    Console.Write("число положительное");
+            //}
+            //else
+            //{
+            //    Console.Write("число отрицательное");
+            //}
+
+
+            //6.Определить, входит и цена выбранного ноутбука в диапазон от 20 до 30 тыс.рублей.
+
+            //Console.Write("введите цену ноутбука: ");
+            //int price = int.Parse(Console.ReadLine());
+            //if (price>20000 & price<30000)
+            //{
+            //    Console.Write("цена входит в диапазон");
+            //}
+            //else
+            //{
+            //    Console.Write("цена не входит в диапазон");
+            //}
+
+
+            //7.Определить, является или нет текущий год високосным? Год ввести с клавиатуры.
+
+            //Console.Write("введите год: ");
+            //int year = int.Parse(Console.ReadLine());
+            //if (year%4==0)
+            //{
+            //    Console.Write("год високосный");
+            //}
+            //else
+            //{
+            //    Console.Write("год не високосный");
+            //}
+
+
+            //доп задания
+
+            //Console.Write("x=");
+            //int x = int.Parse(Console.ReadLine());
+            //if (x > 0)
+            //{
+            //    x++;
+            //}
+            //else
+            //{
+            //    x--;
+            //}
+            //Console.Write(x);
+
+            //int y = x > 0 ? ++x : --x;
+            //Console.Write(y);
+
+
+            //Console.Write("a=");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.WriteLine(a > b ? $"{a} больше" : $"{b} больше");
+
+
+            Console.ReadKey();
+        }
+    }
+}
